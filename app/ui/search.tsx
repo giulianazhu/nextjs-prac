a842f457fe29574d9132a2ed557184133f3a4a9f
Giuliana Zhu <135511633+giulianazhu@users.noreply.github.com>
2024-08-09 11:46:05 +0200

    commit

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { usePathname, useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 import { useDebouncedCallback } from "use-debounce";
 
